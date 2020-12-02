877fff27ef174b97852ef7aeee4bbab4234f7d35
pete <pete@function340.com>
2020-12-02 13:50:44 +0000

    Cleanup

--- day2/day2b.py
+++ day2/day2b.py
@@ -3,13 +3,10 @@ from parse import parse
 
 sdir = os.path.dirname(os.path.realpath(__file__))
 data = [line.strip() for line in open(sdir + "\\day2.txt").readlines()]
-#data = list(map(int, data))
 nvalid = 0
 for d in data:
     (a, b, ch, pw) = parse("{:d}-{:d} {}: {}", d)
-    n = 0
-    if (pw[a-1] == ch): n += 1
-    if (pw[b-1] == ch): n += 1
+    n = (pw[a-1] + pw[b-1]).count(ch)
     if (n == 1):
         nvalid += 1
         print(pw)
