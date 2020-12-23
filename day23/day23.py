78323326c74b4838d53280d66b6bc0b513a0ba77
Pete Bannister <pw.bannister@googlemail.com>
2020-12-23 10:30:48 +0000

    Refactor to use integer

--- day23/day23.py
+++ day23/day23.py
@@ -7,9 +7,9 @@ sdir = os.path.dirname(os.path.realpath(__file__))
 #test = [line.strip() for line in open(sdir + "/test.txt").readlines()]
 
 def move(input, current):
-	index = input.find(current)
-	three = ''
-	res = ''
+	index = input.index(current)
+	three = []
+	res = []
 
 	if (index + 4 <= len(input)):
 		three = input[index + 1:index + 4]
@@ -22,30 +22,31 @@ def move(input, current):
 
 	print("pick up: ", three)
 
-	dest = int(current)
+	dest = current
 	didx = -1
 	while didx < 0:
 		dest = dest - 1
 		if (dest <= 0): # 0 not in either input
 			dest = 9
-		didx = res.find(str(dest))
+		if dest in res:
+			didx = res.index(dest)
 		
 	print("destination: ", dest)
 	res = res[:didx+1] + three + res[didx+1:]
 
 	# current cup must retain its index
-	ni = res.find(current)
+	ni = res.index(current)
 	right = res[ni+1:] + res[:ni]
 	split = len(right)-index
 	if split == 0:
-		res = right + current
+		res = right + [current]
 	else:
-		res = right[split:] + current + right[:split]
+		res = right[split:] + [current] + right[:split]
 
 	return res
 
 def cups_after_1(v):
-	ni = v.find('1')
+	ni = v.index(1)
 	return v[ni+1:] + v[:ni]
 
 
@@ -54,20 +55,20 @@ def solve(input, iterations):
 	r2 = 0
 
 	N = 1
-	input = copy.deepcopy(input)
-	current = input[0]
+	cups = [int(cup) for cup in input]
+	current = cups[0]
 	for i in range(0, iterations):
 		print("move: ", N)
-		print("cups: ", input)
+		print("cups: ", cups)
 		print("current: ", current)
-		input = move(input, current)
-		cidx = input.find(current)
-		current = input[(cidx+1) % len(input)]
+		cups = move(cups, current)
+		cidx = cups.index(current)
+		current = cups[(cidx+1) % len(cups)]
 		N = N + 1
 		print('')
-	print("final: ", input)
+	print("final: ", cups)
 
-	r1 = cups_after_1(input)
+	r1 = ''.join([str(x) for x in cups_after_1(cups)])
 	return (r1, r2)
 
 
